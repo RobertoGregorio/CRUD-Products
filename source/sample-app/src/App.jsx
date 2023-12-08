c8484b7189f562d4bef7b935899df5607e86859a
unknown <robertoxd822@gmail.com>
2023-12-08 01:24:18 -0300

    Meu primeiro projeto em react

---
+++ source/sample-app/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Hello word react!</h1>
+
+      <div className="div-one">
+        <p>Realizando exercicio 1</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
